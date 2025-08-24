2f5cfa59a54a9793207494da36c4a24b02237847
ashrayshetty001 <147970294+ashrayshetty001@users.noreply.github.com>
2025-08-24 19:59:43 +0530

    Create Move_zeroes_to_end.java

---
+++ Move_zeroes_to_end.java
@@ -0,0 +1,16 @@
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                arr[count]=arr[i];
+                count++;
+            }
+            
+        }
+        for(int i=count;i<arr.length;i++){
+            arr[i]=0;
+        }
+    }
+}
